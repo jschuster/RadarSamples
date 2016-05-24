ee9f9d81254c4849b5f0f8290d4e5e55f4a2f508
Joachim Schuster <joachim.schuster@venqoo.com>
2016-05-24 08:51:21 +0200

    added comments.

--- AppIconBug/AppIconBug/ViewController.m
+++ AppIconBug/AppIconBug/ViewController.m
@@ -18,12 +18,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+     /**
+     * Copy or move Safari.app from your Applications folder to your Desktop.
+     * If you run the example open the Console app and filter for appiconbug.
+     * You will find a log of the kind:
+     * 24.05.16 08:42:16,555 sandboxd[129]: ([36785]) AppIconBug(36785) deny file-read-data /Users/xyz/Desktop/Safari.app/Contents/PkgInfo
+     * The icon will be fetched correctly anyway.
+     */
     self.fullPath = [@"~/Desktop/Safari.app" stringByExpandingTildeInPath];
     NSImage* icon = [[NSWorkspace sharedWorkspace] iconForFile:self.fullPath];
     self.imageButton.image = icon;
 }
 
 - (IBAction)buttonClicked:(id)sender {
+    /**
+     * The same App used to open an URL will open the app in that location and shows the given url without any sandboxd warnung.
+     */
     NSError* error;
     NSURL* appUrlFromPath = [NSURL fileURLWithPath:self.fullPath];
     [[NSWorkspace sharedWorkspace] openURLs:@[[NSURL URLWithString:@"http://www.apple.com"]] withApplicationAtURL:appUrlFromPath options:NSWorkspaceLaunchDefault configuration:@{} error:&error];
